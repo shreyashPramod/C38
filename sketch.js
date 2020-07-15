8af2d0e7cdca43058ed85ac1e10e84c88514dad4
shreyashPramod <65612631+shreyashPramod@users.noreply.github.com>
2020-07-15 06:24:10 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,10 +13,10 @@ var track,car1_img,car2_img,car3_img,car4_img;
 
 function preload(){
   track=loadImage("images/track.jpg");
-  car1_img=loadImage("../images/car1.png");
-  car2_img=loadImage("../images/car2.png");
-  car3_img=loadImage("../images/car3.png");
-  car4_img=loadImage("../images/car4.png");
+  car1_img=loadImage("images/car1.png");
+  car2_img=loadImage("images/car2.png");
+  car3_img=loadImage("images/car3.png");
+  car4_img=loadImage("images/car4.png");
   
 }
 
